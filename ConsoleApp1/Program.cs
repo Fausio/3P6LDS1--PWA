495f8eaab207d47d7468f403caad133d49dce43a
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2022-08-15 11:26:41 +0200

    remove all

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
         //              Fazer um programa que, a partir de uma lista de produtos, 
         //            calcule a soma dos preços somente dos produtos cujo nome começa com "T".
 
-        class Product
+        public class Product
         {
             public string name;
             public double price;
@@ -22,35 +22,55 @@ namespace ConsoleApp1
             }
         }
 
-        delegate int delegado(int i);
-
         static void Main(string[] args)
         {
-            delegado meuDelegate = x => x * x;
-            int valor = meuDelegate(5);
+
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            Console.Write("result \n" + total_price_with_T_product(list));
+        }
+
+        public static bool ProductTest(Product p)
+        {
+            return p.price >= 100.0;
+        }
+
+        static string total_price_with_T_product(List<Product> model)
+        { 
+            model.RemoveAll(startWithT);
+            return model.Aggregate(0.0, (x, y) => x + y.price).ToString();
         }
 
 
-        static string total_price_with_T_product(List<Product> model)
+        public static bool startWithT(Product model)
         {
-            // result variable
 
-            List<Product> productResult = new List<Product>();
 
-            model.ForEach(x =>
+            try
             {
                 //  get fist character of string 
-                string fistString = x.name.Substring(0, 1);
+                string fistString = model.name.Substring(0, 1);
 
                 //  sum values of T product name
                 if (fistString.Contains("t") || fistString.Contains("T"))
                 {
-                    productResult.Add(x);
+                    return false;
                 }
+                else
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write("Erro ao tentar determinar a primeira letra ", ex);
+                throw;
+            }
 
-            });
-
-            return productResult.Aggregate(0.0, (x, y) => x + y.price).ToString();
         }
 
     }
