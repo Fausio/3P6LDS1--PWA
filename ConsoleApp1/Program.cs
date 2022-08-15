57b8b18f71b7524559876405b57b9eb355b4cbc7
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2022-08-15 11:02:26 +0200

    update total_price_with_T_product

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
         //        Exercícios:
         //              Fazer um programa que, a partir de uma lista de produtos, 
         //            calcule a soma dos preços somente dos produtos cujo nome começa com "T".
-      
+
         class Product
         {
             public string name;
@@ -24,22 +24,22 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
+
             List<Product> list = new List<Product>();
-            list.Add(new Product("Tv", 900.00));
-            list.Add(new Product("Mouse", 50.00));
-            list.Add(new Product("Tablet", 350.50));
-            list.Add(new Product("HD Case", 80.90));
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+            list.Sort((p1, p2) => p1.name.ToUpper().CompareTo(p2.name.ToUpper()));
 
-
-            Console.WriteLine("Print result: \n " + total_price_with_T_product(list));
-
+            var sum = list.Aggregate(0.0, (x, y) => x + y.price);
         }
 
 
         static string total_price_with_T_product(List<Product> model)
         {
             // result variable
-            double TotalResult = 0;
+
+            List<Product> productResult = new List<Product>();
 
             model.ForEach(x =>
             {
@@ -49,12 +49,12 @@ namespace ConsoleApp1
                 //  sum values of T product name
                 if (fistString.Contains("t") || fistString.Contains("T"))
                 {
-                    TotalResult += x.price;
+                    productResult.Add(x);
                 }
 
             });
 
-            return TotalResult.ToString();
+            return productResult.Aggregate(0.0, (x, y) => x + y.price).ToString();
         }
 
     }
